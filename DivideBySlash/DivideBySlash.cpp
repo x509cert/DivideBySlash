43a8b3510839d605ef48b35b3af046bb1eecf2a1
Michael Howard (CYBER SECURITY) <mikehow@ntdev.microsoft.com>
2024-04-17 09:59:38 -0500

    Add project files.

---
+++ DivideBySlash/DivideBySlash.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <string>
+#include <iostream>
+
+const std::wstring getFilenameFromUser()
+{
+	return L"C:∖SensitiveFiles∖WorldDominationPlans.txt";
+}
+
+std::string ConvertWideToANSI(const std::wstring& wstr)
+{
+	int count = WideCharToMultiByte(CP_OEMCP, 0, wstr.c_str(), wstr.length(), NULL, 0, NULL, NULL);
+	std::string str(count, 0);
+	WideCharToMultiByte(CP_OEMCP, 0, wstr.c_str(), -1, &str[0], count, NULL, NULL);
+	return str;
+}
+
+int main()
+{
+	const std::wstring path = getFilenameFromUser();
+	if (path.find(L"\\") != std::wstring::npos) {
+		std::wcout << L"Found '\\' in the path" << std::endl;
+		return 1;
+	}
+
+	auto str = ConvertWideToANSI(path);
+
+	// Do something sensitive with the path
+	std::cout << str << std::endl;
+
+	std::string ipLH = "lοcalhοst";
+	if (ipLH == "localhost") {
+		std::cout << "You can't access localhost, go away" << std::endl;
+	}
+
+
+	return 0;
+}
